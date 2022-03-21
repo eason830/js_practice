7efad907fef99e45da4dc5adf191b820199eacc5
eason830 <orz0830@gmail.com>
2022-03-21 15:16:00 +0800

    練習

---
+++ js30.js
@@ -0,0 +1,48 @@
+// 第一題
+// function printMany () {
+//   for(let i = 1; i<=100; i++) {
+//     console.log(i)
+//   }
+// }
+
+// printMany()
+
+
+
+//第二題
+// function printEvery3 () {
+//   for(let i = 1;i<=88;i=i+3){
+//     console.log(i);
+//   }
+// }
+// printEvery3()
+
+
+// 第三題
+// function star(n) {
+//   let result="";
+//   for(let i = 0 ; i < n ; i++) {
+//     result += "*";
+//   }
+//   return result
+// }
+// console.log(star(3))
+// console.log(star(10))
+
+
+// 第四題
+// function isUpperCase(str) {
+//   if (str.length == 0) {
+//     return false
+//   } else if (str[0] == str[0].toUpperCase()){
+//     return true
+//   } else {
+//     return false
+//   }
+// }
+// console.log(isUpperCase("ABCD"))
+// console.log(isUpperCase(""))
+// console.log(isUpperCase("aBCD"))
+
+
+// 第五題
